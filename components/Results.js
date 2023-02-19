a54aef2c7b169aa4bad970233f1f3f40ba8f536c
ahmedabdullahfahim <ahmed.abdallah5022@gmail.com>
2023-02-19 18:16:45 +0200

    mobile margins added

--- components/Results.js
+++ components/Results.js
@@ -3,7 +3,7 @@ import FlipMove from 'react-flip-move'
 
 const Results = ({ results }) => {
   return (
-    <FlipMove className='space-x-3 px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3'>
+    <FlipMove className='space-x-3 px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 ml-8 sm:ml-0'>
       {results.map((result) => (
         <Thumbnail key={result.id} result={result} />
       ))}
